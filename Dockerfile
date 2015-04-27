019ffcc1bb86afa6640280de782c6f4811dc5044
Manel Martinez <manel@nixelsolutions.com>
2015-04-27 15:32:08 +0200

    first commit

--- Dockerfile
+++ Dockerfile
@@ -6,19 +6,23 @@ RUN apt-get update && \
     apt-get install -y python-software-properties software-properties-common
 RUN add-apt-repository -y ppa:gluster/glusterfs-3.5 && \
     apt-get update && \
-    apt-get install -y git nodejs nginx supervisor glusterfs-client dnsutils
+    apt-get install -y git nodejs nginx supervisor glusterfs-client dnsutils jq wget
 
 
 ENV GLUSTER_VOL ranchervol
 ENV GLUSTER_VOL_PATH /mnt/${GLUSTER_VOL}
 ENV GLUSTER_PEER **ChangeMe**
+ENV BALANCER asteroids
+ENV RANCHER_SERVER_URL **ChangeMe**
 ENV DEBUG 0
 
-ENV GAME_SERVERS **ChangeMe**
 ENV GAME_PORT 82
 ENV HTTP_CLIENT_PORT 80
 ENV HTTP_SERVER_PORT 81
 ENV HTTP_DOCUMENTROOT ${GLUSTER_VOL_PATH}/asteroids/documentroot
+
+EXPOSE ${HTTP_CLIENT_PORT}
+EXPOSE ${HTTP_PSERVER_PORT}
 
 RUN mkdir -p /var/log/supervisor ${GLUSTER_VOL_PATH}
 WORKDIR ${GLUSTER_VOL_PATH}
@@ -28,8 +32,14 @@ ADD ./bin /usr/local/bin
 RUN chmod +x /usr/local/bin/*.sh
 ADD ./etc/supervisord.conf /etc/supervisor/conf.d/supervisord.conf
 ADD ./etc/nginx/sites-available/asteroids /etc/nginx/sites-available/asteroids
+
 RUN echo "daemon off;" >> /etc/nginx/nginx.conf
 RUN rm -f /etc/nginx/sites-enabled/default
 RUN ln -s /etc/nginx/sites-available/asteroids /etc/nginx/sites-enabled/asteroids
+RUN perl -p -i -e "s/UPSTREAM_SERVERS/   server localhost:${GAME_PORT}/g" /etc/nginx/sites-enabled/asteroids
+RUN perl -p -i -e "s/HTTP_CLIENT_PORT/${HTTP_CLIENT_PORT}/g" /etc/nginx/sites-enabled/asteroids
+RUN perl -p -i -e "s/HTTP_SERVER_PORT/${HTTP_SERVER_PORT}/g" /etc/nginx/sites-enabled/asteroids
+RUN perl -p -i -e "s/HTTP_DOCUMENTROOT/${HTTP_DOCUMENTROOT}/g" /etc/nginx/sites-enabled/asteroids
+RUN perl -p -i -e "s/GAME_PORT/${GAME_PORT}/g" /etc/supervisord.conf
 
 CMD ["/usr/local/bin/run.sh"]
--- bin/run.sh
+++ bin/run.sh
@@ -9,20 +9,33 @@ if [ "${GLUSTER_PEER}" == "**ChangeMe**" ]; then
    exit 0
 fi
 
+if [ "${RANCHER_SERVER_URL}" != "**ChangeMe**" ]; then
+   # Stuff for active-passive load balancing because game does not share sessions across containers
+   # Unused on 1.0 version
+   # Get load balancer data
+   lbconfig=`wget -O- "${RANCHER_SERVER_URL}/v1/loadbalancers"`
+   lb_id=`echo $lbconfig | jq -r '.data | .[] | select(. name=="${BALANCER}") | .id'`
+   lbcontainers=`wget -O- "${RANCHER_SERVER_URL}/v1/loadbalancers/$lb_id/loadbalancertargets"`
+   lb_instances=`echo $lbcontainers | jq -r '.data | .[] | .instanceId'`
+   echo "upstream nodejs {" > ${HTTP_DOCUMENTROOT}/../lb.instances
+   for instance in $lb_instances; do
+       instance=`wget -O- "${RANCHER_SERVER_URL}/v1/instances/$instance"`
+       instance_ip=`echo $instance | jq -r '.primaryIpAddress'`
+       echo "   server $instance_ip:${GAME_PORT};" >> ${HTTP_DOCUMENTROOT}/../lb.instances
+   done
+   echo "}" >> ${HTTP_DOCUMENTROOT}/../lb.instances
+fi
+
 echo "=> Mounting GlusterFS volume ${GLUSTER_VOL} from cluster ${GLUSTER_PEER}..."
 ping -c 10 ${GLUSTER_PEER} >/dev/null 2>&1
-mount -t glusterfs ${GLUSTER_PEER}:/${GLUSTER_VOL}  /mnt/${GLUSTER_VOL}
-#glusterfs --volfile-server=${GLUSTER_PEER} --volfile-id=/${GLUSTER_VOL} /mnt/${GLUSTER_VOL}
+mount -t glusterfs ${GLUSTER_PEER}:/${GLUSTER_VOL} ${GLUSTER_VOL_PATH}
 
-if [ ! -d /mnt/${GLUSTER_VOL}/asteroids ]; then
-   echo "=> Setting up asteroids game..."
-   pushd /mnt/${GLUSTER_VOL}
-   git clone https://github.com/BonsaiDen/NodeGame-Shooter.git
-   mv NodeGame-Shooter asteroids
-   popd
+if [ ! -d ${HTTP_DOCUMENTROOT} ]; then
+   git clone https://github.com/BonsaiDen/NodeGame-Shooter.git ${HTTP_DOCUMENTROOT}
 fi
-my_public_ip=`dig -4 @ns1.google.com -t txt o-o.myaddr.l.google.com +short | sed "s/\"//g"`   
-perl -p -i -e "s/HOST = '.*'/HOST = '$my_public_ip'/g" /mnt/${GLUSTER_VOL}/asteroids/client/config.js
-perl -p -i -e "s/PORT = .*;/PORT = 81;/g" /mnt/${GLUSTER_VOL}/asteroids/client/config.js
+
+my_public_ip=`dig -4 @ns1.google.com -t txt o-o.myaddr.l.google.com +short | sed "s/\"//g"`
+perl -p -i -e "s/HOST = '.*'/HOST = '${my_public_ip}'/g" ${HTTP_DOCUMENTROOT}/client/config.js
+perl -p -i -e "s/PORT = .*;/PORT = ${HTTP_SERVER_PORT};/g" ${HTTP_DOCUMENTROOT}/client/config.js
 
 /usr/bin/supervisord
